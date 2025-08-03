cb8b8213cd30f310db2e2bcaafc7e87d085966b7
Anton Example <youremail@example.com>
2025-08-03 16:56:34 +0200

    Update

--- src/Main.java
+++ src/Main.java
@@ -5,36 +5,28 @@ public class Main {
 
         Random random = new Random();
 
-        double[] array = new double[3];
+        double[] array = new double[10];
 
         for (int c = 0 ; c < array.length ; c++){
-
             array[c] = Math.random();
-
         }
 
         System.out.println(getArrayToString(array));
 
-
         int num1;
         int num2;
-
         double numA1;
         double numA2;
 
+        int count = 0;
+        int correctCount;
+
         while (true){
 
+            correctCount = 0;
 
-            num1 = 0;
-            num2 = 0;
-            numA1 = 0;
-            numA2 = 0;
-
-
-
-
-            num1 = random.nextInt(0,3);
-            num2 = random.nextInt(0,3);
+            num1 = random.nextInt(0,10);
+            num2 = random.nextInt(0,10);
 
             numA1 = array[num1];
             numA2 = array[num2];
@@ -42,47 +34,51 @@ public class Main {
             array[num1] = numA2;
             array[num2] = numA1;
 
+            for (int c = 0 ; c < array.length ; c++){
 
+                if (c < array.length-1){
+                    if (array[c] < array[c+1]){
 
+                        correctCount++;
 
-
-
-
-            if (array[0] > array[1] && array[1] > array[2]){
-
-                break;
-
-
+                    }
+                }
             }
 
+            if (correctCount == array.length-1){
+                break;
+            }
+
+            count++;
 
         }
 
         System.out.println(getArrayToString(array));
 
+        System.out.println("It took " + count + " swaps");
 
     }
-
-
-
-
+    
     public static String getArrayToString(double[] array){
 
         String s = "[";
 
         for (int c = 0 ; c < array.length ; c++){
 
-            s += array[c];
-            s += " , ";
+            if (c < array.length-1){
 
+                s += array[c];
+                s += " , ";
+
+            }
+            else {
+
+                s += array[c];
+
+            }
         }
-
         s += "]";
-
         return s;
-
     }
-
-
 
 }
